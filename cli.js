3b7404ed39dc5807c6f1caf6b26756f6c656b5d4
Livio <livio.brunner.lb1@gmail.com>
2018-11-29 23:17:57 +0100

    Force user to enter packages parameter

--- cli.js
+++ cli.js
@@ -1,9 +1,12 @@
+#!/usr/bin/env node
 const { tryPackage } = require('./index');
-
+const log = require('loglevel');
 
 // Parse command line arguments
 const argv = require('minimist')(process.argv.slice(2))
 
 const packages = argv._;
+
+!packages.length && (log.error('You must enter the packages you want to use') || process.exit(1));  
 
 tryPackage(packages);
